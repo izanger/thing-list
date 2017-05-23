b4a54411f7297e76f1bef74c9d0dabef6e71ab09
Ian Zanger <izanger@purdue.edu>
2017-05-23 11:15:34 -0400

    Add basic Header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <header>
+            <h1>ThingList</h1>
+            <h2>So Many Things</h2>
+        </header>
+    )
+}
+export default Header
