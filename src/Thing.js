0f588cdcd9873b1a30626423ee9ee01d33e37611
Ian Zanger <izanger@purdue.edu>
2017-05-23 12:00:46 -0400

    Make Things prettier

--- src/Thing.js
+++ src/Thing.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import './Thing.css'
 const Thing = ({ thing }) => {
     return (
-        <li className="thing">{thing.name}</li>
+        <li className="Thing">
+            <input type="checkbox" value="on"/>
+          <div className="details">
+            <div className="name">
+               {thing.name}
+            </div>
+            <span className="actions">
+              <button className="remove">
+                <i className="fa fa-trash-o"></i>
+              </button>
+            </span>
+          </div>
+        </li>
     )
 }
 export default Thing
